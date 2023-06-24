4d6484aba7433f297267c0e23aaffe66ada6b17a
Sahyam Chakraborty <133288211+sahyam2023@users.noreply.github.com>
2023-06-24 21:51:16 +0530

    Update lecture3.cpp

--- lecture3.cpp
+++ lecture3.cpp
@@ -1,1 +1,1 @@
-#NO code is done in lecture 3 and video title doesn't match with video content as video title was about funtions continued and video was about git
+#NO code is done in lecture 3 and video title doesn't match with video content as video title was about funtions continued and video was about mingw c++ setup
