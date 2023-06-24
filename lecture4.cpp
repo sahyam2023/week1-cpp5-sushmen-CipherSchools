285b0414f60c276462e5fa408c35f68216524deb
Sahyam Chakraborty <133288211+sahyam2023@users.noreply.github.com>
2023-06-24 22:33:13 +0530

    Update lecture4.cpp

--- lecture4.cpp
+++ lecture4.cpp
@@ -1,10 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
+//iterative fun
+//int power(int base, int pow)
+//{
+//int num=1;
+//for(int i=0; i<pow,i++){
+//}
+//recursive fun
 
 int power(int base, int pow){
     if(pow==0)
         return 1;
-        int Smlans = power(base, pow-1);
+        int Smlans = power(base, pow-1); //mlAns -> 2^2
         return Smlans*base;
     
 }
